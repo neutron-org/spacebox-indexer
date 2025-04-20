9fb9c705b3ecc8cff785c6d52f31cc968b805bda
David Uhlmann <dib542@gmail.com>
2025-04-20 22:40:00 +1000

    fix: replace vault config tables with one that can handle all updates

---
+++ migrations/clickhouse/000027_dex_vaults_config.up.sql
@@ -0,0 +1,51 @@
+
+-- spacebox.dex_vaults_config_parts table
+
+CREATE TABLE spacebox.dex_vaults_config_tx_event
+(
+    `timestamp`                 DateTime,
+    `height`                    Int64,
+    `txhash`                    String,
+    `event_index`               Int16,
+    `signer`                    String,
+    `contract_address`          String,
+    `action`                    String,
+    `attributes`                String,
+    -- add index for quick joins when finding related configs
+    INDEX `contract_address_index` (`contract_address`) TYPE bloom_filter
+)
+-- use ReplacingMergeTree ensure (eventually) no duplicates of the ORDER BY columns
+ENGINE = ReplacingMergeTree()
+ORDER BY (
+    `timestamp`,
+    `height`,
+    `txhash`,
+    `event_index`
+)
+SETTINGS index_granularity = 8192;
+
+CREATE MATERIALIZED VIEW spacebox.dex_vaults_config_tx_event_writer TO spacebox.dex_vaults_config_tx_event (
+    `timestamp`                 DateTime,
+    `height`                    Int64,
+    `txhash`                    String,
+    `event_index`               Int16 DEFAULT 0,
+    `signer`                    String,
+    `contract_address`          String,
+    `action`                    String,
+    `attributes`                String
+) AS
+SELECT
+    `timestamp`,
+    `height`,
+    `txhash`,
+    `event_index`,
+    `signer`,
+    `contract_address`,
+    `action`,
+    `attributes`
+FROM spacebox.wasm_txs_events
+WHERE `action` in (
+    'instantiate IMM',
+    'update_config',
+    'create_token'
+)

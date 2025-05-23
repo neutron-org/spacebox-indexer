f34efff4165196926a216b046f1d26528474cccd
David Uhlmann <dib542@gmail.com>
2025-05-23 21:26:30 +1000

    feat: add new DEX DepositLP attributes and derived swap volumes

--- migrations/clickhouse/000023_dex_message_event_deposit_lp.up.sql
+++ migrations/clickhouse/000023_dex_message_event_deposit_lp.up.sql
@@ -20,8 +20,15 @@ CREATE TABLE spacebox.dex_message_event_deposit_lp
     `TokenOne`              LowCardinality(String),
     `TickIndex`             Int64,
     `Fee`                   UInt64,
+    -- AmountIn first appears in mainnet height 22,061,642
+    `AmountInTokenZero`     UInt256 DEFAULT `ReservesZeroDeposited`,
+    `AmountInTokenOne`      UInt256 DEFAULT `ReservesOneDeposited`,
     `ReservesZeroDeposited` UInt256,
     `ReservesOneDeposited`  UInt256,
+    `ReservesZeroSwappedIn` UInt256 DEFAULT 0,
+    `ReservesOneSwappedIn`  UInt256 DEFAULT 0,
+    `ReservesZeroSwappedOut` UInt256 DEFAULT 0,
+    `ReservesOneSwappedOut` UInt256 DEFAULT 0,
     `SharesMinted`          UInt256,
     -- add index for timeseries queries
     INDEX `timestamp_index` (`timestamp`) TYPE minmax,
@@ -58,11 +65,21 @@ CREATE MATERIALIZED VIEW spacebox.dex_message_event_deposit_lp_writer TO spacebo
     `TokenOne`              LowCardinality(String),
     `TickIndex`             Int64,
     `Fee`                   UInt64,
+    `AmountInTokenZero`     UInt256,
+    `AmountInTokenOne`      UInt256,
     `ReservesZeroDeposited` UInt256,
     `ReservesOneDeposited`  UInt256,
+    `ReservesZeroSwappedIn` UInt256,
+    `ReservesOneSwappedIn`  UInt256,
+    `ReservesZeroSwappedOut` UInt256,
+    `ReservesOneSwappedOut` UInt256,
     `SharesMinted`          UInt256
 ) AS
-    WITH JSONExtractArrayRaw(`attributes`) as `event_attributes`
+    WITH
+        JSONExtractArrayRaw(`attributes`) as `event_attributes`,
+        -- get possibly defined (since Neutron v6.0) attributes
+        JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'AmountInTokenZero'), `event_attributes`), 'value') AS `AmountInTokenZeroString`,
+        JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'AmountInTokenOne'), `event_attributes`), 'value') AS `AmountInTokenOneString`
     SELECT
         `timestamp`,
         `height`,
@@ -78,8 +95,14 @@ CREATE MATERIALIZED VIEW spacebox.dex_message_event_deposit_lp_writer TO spacebo
         JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'TokenOne'), `event_attributes`), 'value') AS `TokenOne`,
         toInt64(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'TickIndex'), `event_attributes`), 'value')) AS `TickIndex`,
         toUInt64(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'Fee'), `event_attributes`), 'value')) AS `Fee`,
+        if(notEmpty(`AmountInTokenZeroString`), toUInt256OrZero(`AmountInTokenZeroString`), `ReservesZeroDeposited`) AS `AmountInTokenZero`,
+        if(notEmpty(`AmountInTokenOneString`), toUInt256OrZero(`AmountInTokenOneString`), `ReservesOneDeposited`) AS `AmountInTokenOne`,
         toUInt256OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'ReservesZeroDeposited'), `event_attributes`), 'value')) AS `ReservesZeroDeposited`,
         toUInt256OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'ReservesOneDeposited'), `event_attributes`), 'value')) AS `ReservesOneDeposited`,
+        if (`AmountInTokenZero` > `ReservesZeroDeposited`, toUInt256(`AmountInTokenZero` - `ReservesZeroDeposited`), 0) AS `ReservesZeroSwappedIn`,
+        if (`AmountInTokenOne` > `ReservesOneDeposited`, toUInt256(`AmountInTokenOne` - `ReservesOneDeposited`), 0) AS `ReservesOneSwappedIn`,
+        if (`AmountInTokenZero` < `ReservesZeroDeposited`, toUInt256(`ReservesZeroDeposited` - `AmountInTokenZero`), 0) AS `ReservesZeroSwappedOut`,
+        if (`AmountInTokenOne` < `ReservesOneDeposited`, toUInt256(`ReservesOneDeposited` - `AmountInTokenOne`), 0) AS `ReservesOneSwappedOut`,
         toUInt256OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'SharesMinted'), `event_attributes`), 'value')) AS `SharesMinted`
     FROM spacebox.dex_message_event_action
     WHERE `action` = 'DepositLP'

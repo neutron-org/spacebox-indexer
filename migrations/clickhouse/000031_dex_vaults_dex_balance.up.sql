8a579db47580d32707fc836a4294103f7597c62b
David Uhlmann <dib542@gmail.com>
2025-06-22 12:01:50 +1000

    fix: update price columns of dex_deposit rows

--- migrations/clickhouse/000031_dex_vaults_dex_balance.up.sql
+++ migrations/clickhouse/000031_dex_vaults_dex_balance.up.sql
@@ -113,8 +113,8 @@ SELECT
     `deposited_tuple`.2 AS `token_1_balance`,
     toUInt128OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'token_0_balance'), `event_attributes`), 'value')) AS `intended_token_0_balance`,
     toUInt128OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'token_1_balance'), `event_attributes`), 'value')) AS `intended_token_1_balance`,
-    toFloat32OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'token_0_price'), `event_attributes`), 'value')) AS `token_0_price`,
-    toFloat32OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'token_1_price'), `event_attributes`), 'value')) AS `token_1_price`,
+    toFloat32OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'price_0'), `event_attributes`), 'value')) AS `token_0_price`,
+    toFloat32OrZero(JSONExtractString(arrayFirst(x -> (JSONExtractString(x, 'key') = 'price_1'), `event_attributes`), 'value')) AS `token_1_price`,
     if(`token_1_price` > 0, `token_0_price` / `token_1_price`, `price_ratio`) AS `price_0_to_1`
 FROM spacebox.dex_message_event
 ARRAY JOIN (

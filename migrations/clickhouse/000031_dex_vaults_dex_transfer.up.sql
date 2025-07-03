98fdc43a8374d22efc76222e70ef873c8cf2b97e
David Uhlmann <dib542@gmail.com>
2025-07-03 14:29:44 +1000

    fix: use correct setting on MV

--- migrations/clickhouse/000031_dex_vaults_dex_transfer.up.sql
+++ migrations/clickhouse/000031_dex_vaults_dex_transfer.up.sql
@@ -395,7 +395,7 @@ ARRAY JOIN (
 ) AS `event_tuple`
 SETTINGS
   -- this query can have trouble backfilling with a lot of blocks
-  max_insert_block_size = 10000 -- to height 28000000: Peak memory usage: 94.64 GiB.
+  max_block_size = 10000
 ;
 
 
